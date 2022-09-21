5ddf9b70d65f37632366455963b21fe4a7b60e64
Roger Wells <rsw359@gmail.com>
2022-09-20 18:56:42 -0700

    mountain animation complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import mountains from "./assets/mountains.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="jily-logo">Jilly Cafe</h1>
-      </header>
+      <div class="box">
+        <div class="hid-box">
+          <div id="logo">Jilly</div>
+          <img src={mountains} alt="mtns" />
+        </div>
+      </div>
     </div>
   );
 }
