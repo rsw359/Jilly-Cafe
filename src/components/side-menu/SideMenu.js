13196cda63a76839c382bf0ea8c41250fecd7b9c
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-10-18 23:22:17 -0700

    useEffect added

--- src/components/side-menu/SideMenu.js
+++ src/components/side-menu/SideMenu.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import sideMenuStyles from "./SideMenu.module.css";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 const SideMenu = ({ showMenu, openMenu }) => {
+
+  const [secondRender, setSecondRender] = useState(false);
+
+  useEffect(() => {
+    if (showMenu) {
+      setSecondRender(true);
+    }
+  }, [showMenu]);
+  
   return (
     <div className={showMenu ? "shown" : "hidden"}>
       {
