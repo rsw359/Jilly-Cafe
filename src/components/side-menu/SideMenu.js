811834d6df428d9f193a1ffbacbd61a83e1e724a
Roger Wells <rsw359@gmail.com>
2022-10-26 15:07:46 +0900

    menu items added

--- src/components/side-menu/SideMenu.js
+++ src/components/side-menu/SideMenu.js
@@ -1,9 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import sideMenuStyles from "./SideMenu.module.css";
-import { IoMdMenu, IoMdClose } from "react-icons/io";
+import {
+  IoMdImage,
+  IoIosIceCream,
+  IoMdContacts,
+  IoLogoInstagram,
+  IoMdMenu,
+  IoMdClose,
+} from "react-icons/io";
 
 const SideMenu = ({ showMenu, openMenu }) => {
-
   const [secondRender, setSecondRender] = useState(false);
 
   useEffect(() => {
@@ -15,20 +22,59 @@ const SideMenu = ({ showMenu, openMenu }) => {
   return (
     <div
       className={
-        showMenu 
-        ? sideMenuStyles['shown'] 
-        : secondRender
-          ? sideMenuStyles['hidden']
-          : sideMenuStyles['first-render']
+        showMenu
+          ? sideMenuStyles["shown"]
+          : secondRender
+          ? sideMenuStyles["hidden"]
+          : sideMenuStyles["first-render"]
       }
     >
-      <div className={sideMenuStyles['menu-icon']} onClick={openMenu}>
+      <div className={sideMenuStyles["menu-icon"]} onClick={openMenu}>
         {showMenu ? (
-          
-          <IoMdClose className={sideMenuStyles['menu-close']} />
-          ) : (
-          <IoMdMenu className={sideMenuStyles['menu-open']}/>
+          <IoMdClose className={sideMenuStyles["menu-close"]} />
+        ) : (
+          <IoMdMenu className={sideMenuStyles["menu-open"]} />
         )}
+      </div>
+      <div>
+        <nav>
+          <ul
+            className={
+              showMenu
+                ? sideMenuStyles["ul-item"]
+                : sideMenuStyles["ul-item-hidden"]
+            }
+          >
+            <li
+              className={
+                showMenu ? sideMenuStyles["li-item"] : sideMenuStyles["li-hide"]
+              }
+            >
+              <IoIosIceCream />
+              <Link className={sideMenuStyles["link-text"]} to="/">
+                Shop
+              </Link>
+            </li>
+            <li className={sideMenuStyles["li-item"]}>
+              <IoMdImage />
+              <Link className={sideMenuStyles["link-text"]} to="/">
+                Photos
+              </Link>
+            </li>
+            <li className={sideMenuStyles["li-item"]}>
+              <IoMdContacts />
+              <Link className={sideMenuStyles["link-text"]} to="/">
+                Contact
+              </Link>
+            </li>
+            <li className={sideMenuStyles["li-item"]}>
+              <IoLogoInstagram />
+              <Link className={sideMenuStyles["link-text"]} to="/">
+                Instagram
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
